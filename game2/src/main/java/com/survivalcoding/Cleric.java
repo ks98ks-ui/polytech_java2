57f2157b30049a3f12c90e3a93ee50a04f7db58e
Sunjin.Kim <ks42ks@naver.com>
2026-06-08 12:44:49 +0900

    연습문제3

--- game2/src/main/java/com/survivalcoding/Cleric.java
+++ game2/src/main/java/com/survivalcoding/Cleric.java
@@ -2,10 +2,23 @@ package com.survivalcoding;
 
 public class Cleric {
     String name = "Cleric";
+    final int MaxHP = 50;
     int HP = 50;
+    final int MaxMP = 10;
     int MP = 10;
 
     void selfAid() {
-        System.out.println("MP를 5 소비하는것으로 자신의 HP를 최대 HP까지 회복할 수 있다.");
+        MP -= 5;
+        HP = MaxHP;
+        System.out.println("성직자는 '셀프에이드' 라는 마법을 사용할 수 있고 MP를 5 소비하는것으로 자신의 HP를 최대 HP까지 회복할 수 있다.");
+    }
+
+    int pray(int sec) {
+        Random rand = new Random();
+        int recover = sec + rand.nextInt(3);
+        int actualRecover = Math.min(this.MaxMP - this.MP, recover);
+        this.MP += actualRecover;
+        return actualRecover;
+
     }
 }
