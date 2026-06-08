4f1cfe1790ef98e968cfb5d23a65f08a86dbcd5e
Sunjin.Kim <ks42ks@naver.com>
2026-06-08 11:54:50 +0900

    연습문제2

--- game2/src/main/java/com/survivalcoding/Cleric.java
+++ game2/src/main/java/com/survivalcoding/Cleric.java
@@ -4,4 +4,8 @@ public class Cleric {
     String name = "Cleric";
     int HP = 50;
     int MP = 10;
+
+    void selfAid() {
+        System.out.println("MP를 5 소비하는것으로 자신의 HP를 최대 HP까지 회복할 수 있다.");
+    }
 }
