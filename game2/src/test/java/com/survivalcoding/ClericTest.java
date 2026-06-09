8224fa8c6e007388f456e17e2d969b5ff84e9063
Sunjin.Kim <ks42ks@naver.com>
2026-06-09 14:51:46 +0900

    연습문제 테스트코드

--- game2/src/test/java/com/survivalcoding/ClericTest.java
+++ game2/src/test/java/com/survivalcoding/ClericTest.java
@@ -12,7 +12,7 @@ class ClericTest {
     @DisplayName("selfAid를 하면 mp가 5 소모되고 hp가 최대(50)가 된다")
     void selfAid() {
         // Given
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("클레릭", 10, 10);
         cleric.HP = 10;
         cleric.MP = 10;
 
@@ -28,7 +28,7 @@ class ClericTest {
     @DisplayName("pray를 하면 기도한 시간(초)과 랜덤 값을 더해 MP를 회복한다.")
     void pray() {
         // Given
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("클레릭");
         cleric.MP = 5;
         int sec = 3;
 
@@ -49,5 +49,48 @@ class ClericTest {
         // 최대 MP 10을 넘으면 안 됨
         assertTrue(cleric.MP <= 10,
                 "MP는 최대 MP(10)를 넘으면 안 됩니다.");
+    }
+
+    @Test
+    @DisplayName("MaxHP, MaxMP가 필드의 공유 되었습니다.")
+    void testHPMP() {
+        Cleric cleric = new Cleric("name");
+
+
+        assertEquals(50, Cleric.MaxHP);
+        assertEquals(10, Cleric.MaxMP);
+    }
+
+    @Test
+    @DisplayName("이름이 생성이 잘 되었습니다.")
+    void con() {
+        int test1 = 40;
+        int test2 = 5;
+        int test3 = 35;
+        //Given
+        Cleric cleric = new Cleric("아서스", test1, test2);
+
+
+        //Then
+        assertEquals("아서스", cleric.name);
+        assertEquals(40, cleric.HP);
+        assertEquals(5, cleric.MP);
+
+        //Given
+        Cleric cleric1 = new Cleric("아서스", test3);
+
+        //Then
+        assertEquals("아서스", cleric1.name);
+        assertEquals(35, cleric1.HP);
+        assertEquals(10, Cleric.MaxMP);
+
+        //Given
+        Cleric cleric2 = new Cleric("아서스");
+
+        //Then
+        assertEquals("아서스", cleric2.name);
+        assertEquals(50, Cleric.MaxHP);
+        assertEquals(10, Cleric.MaxMP);
+
     }
 }
