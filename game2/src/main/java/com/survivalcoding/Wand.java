aa77b7c5cfb83118df2d50ec0722d67ca4e6420a
Sunjin.Kim <ks42ks@naver.com>
2026-06-15 13:30:14 +0900

    연습문제1

---
+++ game2/src/main/java/com/survivalcoding/Wand.java
@@ -0,0 +1,29 @@
+package com.survivalcoding;
+
+public class Wand {
+    private String name;
+    private double power;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("이름은 null이 아니어야 함");
+        }
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("이름이 3단어 이상이여야 함");
+        }
+        this.name = name;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower() {
+        if (power >= 0.5 && power <= 100)
+            this.power = power;
+    }
+}
--- game2/src/main/java/com/survivalcoding/Wizard.java
+++ game2/src/main/java/com/survivalcoding/Wizard.java
@@ -1,4 +1,63 @@
 package com.survivalcoding;
 
 public class Wizard {
+    private int hp;
+    private int mp;
+    private String name;
+    private Wand wand;
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        if (hp < 0) {
+            hp = 0;
+        }
+        this.hp = hp;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public void setMp(int mp) {
+        if (mp < 0) {
+            throw new IllegalArgumentException("마법사의 MP는 0 이상이여야 한다");
+        }
+        this.mp = mp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("이름은 null이 아니어야 함");
+        }
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("이름이 3단어 이상이여야 함");
+        }
+
+
+        this.name = name;
+    }
+
+    public Wand getWand() {
+        return wand;
+    }
+
+    public void setWand(Wand wand) {
+        if (wand == null) {
+            throw new IllegalArgumentException("마법사의 지팡이는 null일 수 없다");
+        }
+        this.wand = wand;
+    }
+
+    void heal(Hero hero) {
+        int basePoint = 10;
+        int recovPoint = (int) (basePoint * this.wand.getPower());
+        hero.setHp(hero.getHp() + recovPoint);
+    }
 }
